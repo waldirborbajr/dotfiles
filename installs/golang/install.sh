fab4972077e7e7f3ea44174457bc286c45d4a979
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-06-13 15:32:10 -0300

    feat: goreleaser

--- installs/golang/install.sh
+++ installs/golang/install.sh
@@ -17,7 +17,7 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 go install github.com/cosmtrek/air@latest
 go install github.com/jesseduffield/lazydocker@latest
 go install github.com/jesseduffield/lazygit@latest
-go install github.com/goreleaser/goreleaser@latest
+go install github.com/aser/goreleaser@latest
 go install github.com/nametake/golangci-lint-langserver@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 go install github.com/maaslalani/slides@latest
@@ -26,5 +26,6 @@ go install github.com/spf13/cobra-cli@latest
 env CGO_ENABLED=0 go install -ldflags="-s -w" github.com/gokcehan/lf@latest
 go install github.com/dundee/gdu/v5/cmd/gdu@latest
 go install -v github.com/incu6us/goimports-reviser/v3@latest
+
 
 
