67cc5967baae9e7165aeed5b5166950663a9967e
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-11-16 08:13:32 -0300

    Rand Message: /nippy-drum

--- functions.zsh
+++ functions.zsh
@@ -19,6 +19,7 @@ function getgo(){
   wget https://go.dev/dl/go${version}.linux-amd64.tar.gz
   sudo rm -rf /usr/local/go
   sudo tar -C /usr/local -xzf go${version}.linux-amd64.tar.gz
+  rm go${version}.linux-amd64.tar.gz
 }
 
 # Clean system.
