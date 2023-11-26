78f6f908aab78122af594b0ebd8f2324b90c8081
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-11-26 10:57:28 -0300

    Rand Message: /old-fashioned-hole

--- options.zsh
+++ options.zsh
@@ -61,3 +61,6 @@ ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 # See https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md#how-to-tweak-it
 typeset -A ZSH_HIGHLIGHT_STYLES
 ZSH_HIGHLIGHT_STYLES[comment]='fg=242'
+
+# Rust debug for tracing and other logging
+export RUST_LOG=debug
