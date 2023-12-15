279118ce3cfc25791410c7af4f6ee5da3ef7f351
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-15 16:24:18 -0300

    Rand Message: /flowery-zipper

--- .config/wezterm/utils/background.lua
+++ .config/wezterm/utils/background.lua
@@ -2,16 +2,16 @@ local M = {}
 local h = require("utils/helpers")
 
 M.get_background = function()
-  return {
-    source = {
-      Gradient = {
-        colors = { h.is_dark() and "#000000" or "#ffffff" },
-      },
-    },
-    width = "100%",
-    height = "100%",
-    opacity = h.is_dark() and 0.95 or 0.7,
-  }
+	return {
+		source = {
+			Gradient = {
+				colors = { h.is_dark() and "#000000" or "#ffffff" },
+			},
+		},
+		width = "100%",
+		height = "100%",
+		opacity = h.is_dark() and 0.90 or 0.7,
+	}
 end
 
 return M
