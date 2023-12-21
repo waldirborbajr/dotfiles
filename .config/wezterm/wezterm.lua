58e23f53e75cee9188bd1d098bcad32d91fb6dab
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-20 22:31:45 -0300

    Rand Message: /fearless-camp

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,9 +8,20 @@
 --- My Wezterm config file
 
 local wezterm = require("wezterm")
+local platform = require('utils.platform')()
 local act = wezterm.action
 
 local config = {}
+
+local mod = {}
+
+if platform.is_mac then
+   mod.SUPER = 'SUPER'
+   mod.SUPER_REV = 'SUPER|CTRL'
+elseif platform.is_win or platform.is_linux then
+   mod.SUPER = 'ALT' -- to not conflict with Windows key shortcuts
+   mod.SUPER_REV = 'ALT|CTRL'
+end
 
 -- Use config builder object if possible
 if wezterm.config_builder then config = wezterm.config_builder() end
@@ -45,6 +56,75 @@ config.inactive_pane_hsb = {
 -- Keys
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
+  -- misc/useful --
+  { key = 'F1', mods = 'NONE', action = 'ActivateCopyMode' },
+  { key = 'F2', mods = 'NONE', action = act.ActivateCommandPalette },
+  { key = 'F3', mods = 'NONE', action = act.ShowLauncher },
+  { key = 'F4', mods = 'NONE', action = act.ShowTabNavigator },
+  { key = 'F11', mods = 'NONE', action = act.ToggleFullScreen },
+  { key = 'F12', mods = 'NONE', action = act.ShowDebugOverlay },
+  -- { key = 'f', mods = mod.SUPER, action = act.Search({ CaseInSensitiveString = '' }) },
+
+  -- copy/paste --
+  { key = 'c', mods = 'CTRL|SHIFT', action = act.CopyTo('Clipboard') },
+  { key = 'v', mods = 'CTRL|SHIFT', action = act.PasteFrom('Clipboard') },
+
+  -- tabs --
+  -- tabs: spawn+close
+  { key = 't', mods = mod.SUPER, action = act.SpawnTab('DefaultDomain') },
+  -- { key = 't', mods = mod.SUPER_REV, action = act.SpawnTab({ DomainName = 'WSL:Ubuntu' }) },
+  { key = 'w', mods = mod.SUPER_REV, action = act.CloseCurrentTab({ confirm = false }) },
+
+  -- tabs: navigation
+  { key = '[', mods = mod.SUPER, action = act.ActivateTabRelative(-1) },
+  { key = ']', mods = mod.SUPER, action = act.ActivateTabRelative(1) },
+  { key = '[', mods = mod.SUPER_REV, action = act.MoveTabRelative(-1) },
+  { key = ']', mods = mod.SUPER_REV, action = act.MoveTabRelative(1) },
+
+  -- panes: zoom+close pane
+  { key = 'z', mods = mod.SUPER_REV, action = act.TogglePaneZoomState },
+  { key = 'w', mods = mod.SUPER, action = act.CloseCurrentPane({ confirm = false }) },
+
+  -- panes: navigation
+  { key = 'k', mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Up') },
+  { key = 'j', mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Down') },
+  { key = 'h', mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Left') },
+  { key = 'l', mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Right') },
+
+  -- panes: resize
+  { key = 'UpArrow', mods = mod.SUPER_REV, action = act.AdjustPaneSize({ 'Up', 1 }) },
+  { key = 'DownArrow', mods = mod.SUPER_REV, action = act.AdjustPaneSize({ 'Down', 1 }) },
+  { key = 'LeftArrow', mods = mod.SUPER_REV, action = act.AdjustPaneSize({ 'Left', 1 }) },
+  { key = 'RightArrow', mods = mod.SUPER_REV, action = act.AdjustPaneSize({ 'Right', 1 }) },
+
+  -- fonts --
+  -- fonts: resize
+  { key = 'UpArrow', mods = mod.SUPER, action = act.IncreaseFontSize },
+  { key = 'DownArrow', mods = mod.SUPER, action = act.DecreaseFontSize },
+  { key = 'r', mods = mod.SUPER, action = act.ResetFontSize },
+
+  -- window --
+  -- spawn windows
+  { key = 'n', mods = mod.SUPER, action = act.SpawnWindow },
+  
+  -- panes --
+  -- panes: split panes
+  {
+    key = [[/]],
+    mods = mod.SUPER_REV,
+    action = act.SplitVertical({ domain = 'CurrentPaneDomain' }),
+  },
+  {
+    key = [[\]],
+    mods = mod.SUPER_REV,
+    action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+  },
+  {
+    key = [[-]],
+    mods = mod.SUPER_REV,
+    action = act.CloseCurrentPane({ confirm = true }),
+  },
+    
   -- Send C-a when pressing C-a twice
   { key = "a",          mods = "LEADER|CTRL", action = act.SendKey { key = "a", mods = "CTRL" } },
   { key = "c",          mods = "LEADER",      action = act.ActivateCopyMode },
