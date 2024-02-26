e59a68792f45fc4b03769ef12d0829af45869927
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-02-25 23:00:34 -0300

    feat: add install rust & go utility

---
+++ install.zsh
@@ -0,0 +1,20 @@
+cargo install ripgrep
+cargo install --locked bat
+cargo install xh --locked
+cargo install fd-find
+cargo install --locked zellij
+cargo install alacritty
+cargo install eza
+cargo install exa
+cargo install bottom
+cargo install cargo-cache
+cargo install zoxide --locked
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+git clone https://github.com/jesseduffield/lazygit.git
+cd lazygit
+go install
+
+
