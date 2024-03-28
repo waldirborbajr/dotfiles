11499e22033a7e06bd71e9e8fbe536112365058b
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-03-28 14:29:59 -0300

    chore: from Hack to CommitMono

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,8 +14,8 @@ local act = wezterm.action
 local config = {
   color_scheme = 'catppuccin-frappe',
 
-  font = wezterm.font('FiraCode Nerd Font', { weight = 'Medium', stretch = 'Normal', style = 'Normal' }),
-  font_size = 10.5,
+  font = wezterm.font('CommitNerd Nerd Font', { weight = 'Medium', stretch = 'Normal', style = 'Normal' }),
+  font_size = 11,
   freetype_load_flags = 'DEFAULT',
   -- front_end = 'WebGpu',
 
