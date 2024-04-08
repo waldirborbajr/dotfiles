2f451ed406029fcf0a9611dbffb6d6a5fc4b3fb8
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-04-08 08:33:49 -0300

    chore: add fvim function to find and open file

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,20 +1,28 @@
+# fvim -> find and open a file in vim
+fvim() {
+  if [[ $# -eq 0 ]]; then
+    fd -t f | fzf --header "Open File in Vim" --preview "cat {}" | xargs nvim
+  else
+    fd -t f | fzf --header "Open File in Vim" --preview "cat {}" -q "$@" | xargs nvim
+  fi
+}
 
 # Create python virtual environment
-createVenv(){
+createVenv() {
   # dest="${PWD##*/}"
   dest="$1"
 
-  if python3 -m venv "$dest" ; then
+  if python3 -m venv "$dest"; then
     cd "$dest"
     source "./bin/activate"
     [ ! -f "./requirements.txt" ] || pip install -r requirements.txt
   else
-     echo "Failed to create 'venv' environment" >&2
+    echo "Failed to create 'venv' environment" >&2
   fi
 }
 
 # Download and Instlal GO
-getgo(){
+getgo() {
   version="$1"
   wget https://go.dev/dl/go${version}.linux-amd64.tar.gz
   sudo rm -rf /usr/local/go
@@ -24,8 +32,8 @@ getgo(){
 }
 
 # Clean system.
-clean-system () {
-	rm -rf ~/.cache
+clean-system() {
+  rm -rf ~/.cache
 }
 
 # Choose Zellij Session
@@ -34,17 +42,17 @@ zjsession() {
   NO_SESSIONS=$(echo "${ZJ_SESSIONS}" | wc -l)
 
   if [ "${NO_SESSIONS}" -ge 2 ]; then
-      zellij attach \
+    zellij attach \
       "$(echo "${ZJ_SESSIONS}" | sk)"
   else
-     zellij attach -c
+    zellij attach -c
   fi
 }
 
 # Restart a program.
-refresh () {
-	killall $1
-	$1 &
+refresh() {
+  killall $1
+  $1 &
 }
 
 fii() {
@@ -52,31 +60,30 @@ fii() {
   RG_DEFAULT_COMMAND="rg -i -l --hidden --no-ignore-vcs"
 
   selected=$(
-  FZF_DEFAULT_COMMAND="rg --files" fzf \
-    -m \
-    -e \
-    --ansi \
-    --disabled \
-    --reverse \
-    --bind "ctrl-a:select-all" \
-    --bind "f12:execute-silent:(subl -b {})" \
-    --bind "change:reload:$RG_DEFAULT_COMMAND {q} || true" \
-    --preview "rg -i --pretty --context 2 {q} {}" | cut -d":" -f1,2
+    FZF_DEFAULT_COMMAND="rg --files" fzf \
+      -m \
+      -e \
+      --ansi \
+      --disabled \
+      --reverse \
+      --bind "ctrl-a:select-all" \
+      --bind "f12:execute-silent:(subl -b {})" \
+      --bind "change:reload:$RG_DEFAULT_COMMAND {q} || true" \
+      --preview "rg -i --pretty --context 2 {q} {}" | cut -d":" -f1,2
   )
   [[ -n $selected ]] && nvim $selected # open multiple files in editor
 }
 
 # fkill - kill processes - list only the ones you can kill. Modified the earlier script.
 fkill() {
-    local pid 
-    if [ "$UID" != "0" ]; then
-        pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
-    else
-        pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
-    fi  
+  local pid
+  if [ "$UID" != "0" ]; then
+    pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+  else
+    pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+  fi
 
-    if [ "x$pid" != "x" ]
-    then
-        echo $pid | xargs kill -${1:-9}
-    fi  
+  if [ "x$pid" != "x" ]; then
+    echo $pid | xargs kill -${1:-9}
+  fi
 }
