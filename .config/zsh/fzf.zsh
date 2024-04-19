6a93059189599cc81323a7cda364bb0ae7d11bf7
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-04-19 08:45:50 -0300

    Rand Message: /public-dad

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -4,8 +4,11 @@ export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git --excl
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git --exclude node_modules"
 
-bindkey -r '^T'
-bindkey '^Y' fzf-file-widget
+export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+
+# bindkey -r '^T'
+# bindkey '^Y' fzf-file-widget
 
 # Setup fzf
 # ---------
