5e8ee5935940217a99a528b58f942e9bdd7fc19d
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-04-19 09:29:16 -0300

    Rand Message: /waggish-paint

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -10,6 +10,21 @@ export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
 # bindkey -r '^T'
 # bindkey '^Y' fzf-file-widget
 
+# Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd) fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+    export | unset) fzf --preview "eval 'echo \$'{}" "$@" ;;
+    ssh) fzf --preview 'dig {}' "$@" ;;
+    *) fzf --preview "bat -n --color=always --line-range :500 {}" "$@" ;;
+  esac
+}
+
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/borba/.fzf/bin* ]]; then
