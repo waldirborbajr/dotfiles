56834011270afc96a73fed6ff871cb128fe28597
Waldir Borba Junior <wborbajr@gmail.com>
2024-09-08 20:18:40 -0300

    chore: disable unused plugins

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,88 @@
+return {
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "echasnovski/mini.nvim", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
+  { "stevearc/dressing.nvim", enabled = false },
+
+  -- lsp/init.lua
+  -- lspconfig
+  -- { "neovim/nvim-lspconfig", enabled = false },
+  -- cmdline tools and lsp servers
+  -- { "williamboman/mason.nvim", enabled = false },
+
+  -- coding.lua
+  -- auto completion
+  -- { "hrsh7th/nvim-cmp", enabled = false },
+  -- snippets
+  -- { "nvim-cmp", enabled = false },
+  -- auto pairs
+  { "echasnovski/mini.pairs", enabled = false },
+  -- comments
+  { "folke/ts-comments.nvim", enabled = false },
+  -- Better text-objects
+  { "echasnovski/mini.ai", enabled = false },
+  { "folke/lazydev.nvim", eanbled = false },
+  -- Manage libuv types with lazy.
+  { "Bilal2453/luvit-meta", enabled = false },
+
+  -- colorscheme.lua
+  -- tokyonight
+  { "folke/tokyonight.nvim", enabled = false },
+  -- catppuccin
+  -- { "catppuccin/nvim", enabled = false },
+
+  -- editor.lua
+  -- file explorer
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- search/replace in multiple files
+  { "MagicDuck/grug-far.nvim", enabled = false },
+  -- Flash
+  { "folke/flash.nvim", enabled = false },
+  -- which-key
+  -- { "folke/which-key.nvim", enabled = false },
+  -- git signs
+  { "lewis6991/gitsigns.nvim", enabled = false },
+  -- better diagnostics list and others
+  { "folke/trouble.nvim", enabled = false },
+  -- todo comments
+  { "folke/todo-comments.nvim", enabled = false },
+
+  -- formatting.lua
+  -- { "stevearc/conform.nvim", enabled = false },
+
+  -- linting.lua
+  -- { "mfussenegger/nvim-lint", enabled = false },
+
+  -- treesitter.lua
+  -- Treesitter
+  -- { "nvim-treesitter/nvim-treesitter", enabled = false },
+  -- { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  -- Automatically add closing tags for HTML and JSX
+  { "windwp/nvim-ts-autotag", enabled = false },
+
+  -- ui.lua
+  -- Better `vim.notify()`
+  { "rcarriga/nvim-notify", enabled = false },
+  -- bufferline
+  { "akinsho/bufferline.nvim", enabled = false },
+  -- statusline
+  -- { "nvim-lualine/lualine.nvim", enabled = false },
+  -- indent guides for Neovim
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  -- Highly experimental plugin that completely replaces the UI
+  { "folke/noice.nvim", enabled = false },
+  -- icons
+  -- { "echasnovski/mini.icons", enabled = false },
+  -- ui components
+  { "MunifTanjim/nui.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+
+  -- util.lua
+  -- Session management.
+  { "folke/persistence.nvim", enabled = false },
+  -- library used by other plugins
+  -- { "nvim-lua/plenary.nvim", enabled = false },
+}
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,32 +1,51 @@
 return {
-  {
-    "stevearc/oil.nvim",
-    keys = {
-      {
-        "-",
-        "<CMD>Oil --float<CR>",
-        desc = "Open oil.nvim",
-      },
+  "stevearc/oil.nvim",
+  cmd = { "Oil" },
+  init = function()
+    if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+      require("oil")
+    end
+  end,
+  opts = {
+    keymaps = {
+      ["q"] = "actions.close",
+      ["<C-h>"] = "actions.toggle_hidden",
+      [".."] = "actions.parent",
     },
-    opts = {},
-    -- dependencies = { "nvim-tree/nvim-web-devicons" },
-    dependencies = { "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-    config = function()
-      require("oil").setup({
-        default_file_explorer = true,
-        delete_to_trash = true,
-        skip_confirm_for_simple_edits = true,
-        view_options = {
-          show_hidden = true,
-          natural_order = true,
-          is_always_hidden = function(name, _)
-            return name == ".." or name == ".git"
-          end,
-        },
-        win_options = {
-          wrap = true,
-        },
-      })
-    end,
+  },
+  keys = {
+    { "<leader>o", "<CMD>Oil<CR>", desc = "Open Oil" },
   },
 }
+-- return {
+--   {
+--     "stevearc/oil.nvim",
+--     keys = {
+--       {
+--         "-",
+--         "<CMD>Oil --float<CR>",
+--         desc = "Open oil.nvim",
+--       },
+--     },
+--     opts = {},
+--     -- dependencies = { "nvim-tree/nvim-web-devicons" },
+--     dependencies = { "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+--     config = function()
+--       require("oil").setup({
+--         default_file_explorer = true,
+--         delete_to_trash = true,
+--         skip_confirm_for_simple_edits = true,
+--         view_options = {
+--           show_hidden = true,
+--           natural_order = true,
+--           is_always_hidden = function(name, _)
+--             return name == ".." or name == ".git"
+--           end,
+--         },
+--         win_options = {
+--           wrap = true,
+--         },
+--       })
+--     end,
+--   },
+-- }
