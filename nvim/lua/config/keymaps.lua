ba6be3a0f103d60283056b8740068521ce50ae20
Waldir Borba Junior <wborbajr@gmail.com>
2024-09-09 16:53:41 -0300

    chore: plugin and refactor

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,37 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- This file is automatically loaded by lazyvim.config.init
+local Util = require("lazyvim.util")
+
+-- DO NOT USE THIS IN YOU OWN CONFIG!!
+-- use `vim.keymap.set` instead
+local map = Util.safe_keymap_set
+
+-- Tabs
+map("n", "<tab>", "<CMD>tabNext<CR>", { noremap = true, silent = true, desc = "Next Tab" })
+map("n", "<S-tab>", "<CMD>tabprevious<CR>", { noremap = true, silent = true, desc = "Prevous Tab" })
+map("n", "<C-tab>", "<CMD>tabclose<CR>", { noremap = true, silent = true, desc = "Close Tab" })
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "!!", ":qa!<enter>", { desc = "" })
+map("n", "QQ", ":q!<enter>", { desc = "" })
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+map("n", "WW", ":w!<enter>", { desc = "" })
+map("n", "E", "$", { desc = "" })
+map("n", "B", "^", { desc = "" })
+map("n", "td", ":TodoTelescope<CR>", { desc = "" })
+map("n", "<leader>rs", ":%s/", { desc = "" })
+map("n", "<leader>rw", ":%s/<<C-r><C-w>>/<C-r><C-w>/gI<Left><Left><Left>", { desc = "" })
+map("n", "<leader>gH", "<cmd>OpenGithubRepo<cr>", { desc = "Open GitHub Repo" })
+
+-- Custom navigation
+map("n", "<C-d>", "<C-d>zz", { desc = "" })
+map("n", "<C-u>", "<C-u>zz", { desc = "" })
+map("n", "<C-f>", "<C-f>zz", { desc = "" })
+map("n", "<C-b>", "<C-b>zz", { desc = "" })
--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,11 +1,20 @@
-return {
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "echasnovski/mini.nvim", enabled = false },
-  { "ggandor/flit.nvim", enabled = false },
-  { "goolord/alpha-nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "nvim-pack/nvim-spectre", enabled = false },
-  { "stevearc/dressing.nvim", enabled = false },
+local disabled_plugins = {
+  "SmiteshP/nvim-navic",
+  "echasnovski/mini.diff",
+  "echasnovski/mini.surround",
+  "echasnovski/mini.indentscope",
+  "echasnovski/mini.nvim",
+  "folke/neoconf.nvim",
+  "ggandor/flit.nvim",
+  "ggandor/leap.nvim",
+  -- { "mfussenegger/nvim-dap", enabled = false },
+  "nvim-neo-tree/neo-tree.nvim",
+  "nvim-pack/nvim-spectre",
+  "nvim-treesitter/nvim-treesitter-context",
+  "simrat39/symbols-outline.nvim",
+
+  "goolord/alpha-nvim",
+  "stevearc/dressing.nvim",
 
   -- lsp/init.lua
   -- lspconfig
@@ -21,32 +30,32 @@ return {
   -- auto pairs
   -- { "echasnovski/mini.pairs", enabled = false },
   -- comments
-  { "folke/ts-comments.nvim", enabled = false },
+  "folke/ts-comments.nvim",
   -- Better text-objects
-  { "echasnovski/mini.ai", enabled = false },
-  { "folke/lazydev.nvim", eanbled = false },
+  "echasnovski/mini.ai",
+  "folke/lazydev.nvim",
   -- Manage libuv types with lazy.
-  { "Bilal2453/luvit-meta", enabled = false },
+  "Bilal2453/luvit-meta",
 
   -- colorscheme.lua
   -- tokyonight
-  { "folke/tokyonight.nvim", enabled = false },
+  "folke/tokyonight.nvim",
   -- catppuccin
   -- { "catppuccin/nvim", enabled = false },
 
   -- editor.lua
   -- file explorer
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  "nvim-neo-tree/neo-tree.nvim",
   -- search/replace in multiple files
-  { "MagicDuck/grug-far.nvim", enabled = false },
+  "MagicDuck/grug-far.nvim",
   -- Flash
-  { "folke/flash.nvim", enabled = false },
+  "folke/flash.nvim",
   -- which-key
   -- { "folke/which-key.nvim", enabled = false },
   -- git signs
-  { "lewis6991/gitsigns.nvim", enabled = false },
+  "lewis6991/gitsigns.nvim",
   -- better diagnostics list and others
-  { "folke/trouble.nvim", enabled = false },
+  "folke/trouble.nvim",
   -- todo comments
   -- { "folke/todo-comments.nvim", enabled = false },
 
@@ -61,28 +70,35 @@ return {
   -- { "nvim-treesitter/nvim-treesitter", enabled = false },
   -- { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
   -- Automatically add closing tags for HTML and JSX
-  { "windwp/nvim-ts-autotag", enabled = false },
+  "windwp/nvim-ts-autotag",
 
   -- ui.lua
   -- Better `vim.notify()`
-  { "rcarriga/nvim-notify", enabled = false },
+  "rcarriga/nvim-notify",
   -- bufferline
-  { "akinsho/bufferline.nvim", enabled = false },
+  "akinsho/bufferline.nvim",
   -- statusline
   -- { "nvim-lualine/lualine.nvim", enabled = false },
   -- indent guides for Neovim
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  "lukas-reineke/indent-blankline.nvim",
   -- Highly experimental plugin that completely replaces the UI
-  { "folke/noice.nvim", enabled = false },
+  "folke/noice.nvim",
   -- icons
   -- { "echasnovski/mini.icons", enabled = false },
   -- ui components
-  { "MunifTanjim/nui.nvim", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
+  "MunifTanjim/nui.nvim",
+  "nvimdev/dashboard-nvim",
 
   -- util.lua
   -- Session management.
-  { "folke/persistence.nvim", enabled = false },
+  "folke/persistence.nvim",
   -- library used by other plugins
   -- { "nvim-lua/plenary.nvim", enabled = false },
 }
+
+local lazy_disabled = {}
+for _, plugin in ipairs(disabled_plugins) do
+  lazy_disabled[#lazy_disabled + 1] = { plugin, enabled = false }
+end
+
+return lazy_disabled
