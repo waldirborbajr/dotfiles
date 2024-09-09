e517b498533a36f7e4090e2d924d5bcc0122c2f2
Waldir Borba Junior <wborbajr@gmail.com>
2024-09-08 21:56:18 -0300

    chore: obsidian

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -17,35 +17,3 @@ return {
     { "<leader>o", "<CMD>Oil<CR>", desc = "Open Oil" },
   },
 }
--- return {
---   {
---     "stevearc/oil.nvim",
---     keys = {
---       {
---         "-",
---         "<CMD>Oil --float<CR>",
---         desc = "Open oil.nvim",
---       },
---     },
---     opts = {},
---     -- dependencies = { "nvim-tree/nvim-web-devicons" },
---     dependencies = { "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
---     config = function()
---       require("oil").setup({
---         default_file_explorer = true,
---         delete_to_trash = true,
---         skip_confirm_for_simple_edits = true,
---         view_options = {
---           show_hidden = true,
---           natural_order = true,
---           is_always_hidden = function(name, _)
---             return name == ".." or name == ".git"
---           end,
---         },
---         win_options = {
---           wrap = true,
---         },
---       })
---     end,
---   },
--- }
