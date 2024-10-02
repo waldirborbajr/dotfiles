45968cfb3913a85c01ee369b71e6d1a3829d28d6
Waldir Borba Junior <wborbajr@gmail.com>
2024-10-01 22:33:35 -0300

    chore(wezterm): cleanup config files

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,85 +1,94 @@
-local wezterm = require("wezterm")
-
+local wezterm = require('wezterm')
 local config = {}
+require('events')
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
-config.enable_tab_bar = false -- remove tab bar
-
-config.window_decorations = "RESIZE" -- remove window decorations
-config.window_background_opacity = 0.95
-config.macos_window_background_blur = 10
-
-config.color_scheme = "Catppuccin Frappe"
-
-config.audible_bell = "Disabled" -- disable sounds when at the end of doc
-
--- this is going to be used to toggle between color schemes
--- local function scheme_for_apperance(appearance)
--- 	if appearance:find("Dark") then
--- 		return "Catppuccin Frappe"
--- 	else
--- 		return "Catppuccin Latte"
--- 	end
--- end
---
--- print(wezterm.gui.get_appearance())
-
--- config.color_scheme = scheme_for_apperance(wezterm.gui.get_apperanace()) -- get the current theme
-
-config.font = wezterm.font("MesloLGS Nerd Font", { weight = "Medium", stretch = "Normal", style = "Normal" })
-config.font_size = 12
--- config.font_weight
-config.cursor_blink_rate = 800
-
-config.enable_tab_bar = false
-
-config.inactive_pane_hsb = {
-	saturation = 0.9,
-	brightness = 0.8,
+config = {
+  color_scheme = 'Catppuccin Frappe',
+  default_cursor_style = 'SteadyBar',
+  automatically_reload_config = true,
+  window_close_confirmation = 'NeverPrompt',
+  adjust_window_size_when_changing_font_size = false,
+  window_decorations = 'RESIZE',
+  check_for_updates = false,
+  use_fancy_tab_bar = false,
+  tab_bar_at_bottom = false,
+  font_size = 12,
+  font = wezterm.font('MesloLGS Nerd Font', { weight = 'Medium', stretch = 'Normal', style = 'Normal' }),
+  enable_tab_bar = false,
+  window_padding = {
+    left = 3,
+    right = 3,
+    top = 0,
+    bottom = 0,
+  },
+  background = {
+    {
+      source = {
+        File = '/home/' .. os.getenv('USER') .. '/.config/wezterm/moon.jpg',
+      },
+      hsb = {
+        hue = 1.0,
+        saturation = 1.02,
+        brightness = 0.25,
+      },
+      -- attachment = { Parallax = 0.3 },
+      -- width = "100%",
+      -- height = "100%",
+    },
+    {
+      source = {
+        Color = '#282c35',
+      },
+      width = '100%',
+      height = '100%',
+      opacity = 0.85,
+    },
+  },
+  -- from: https://akos.ma/blog/adopting-wezterm/
+  hyperlink_rules = {
+    -- Matches: a URL in parens: (URL)
+    {
+      regex = '\\((\\w+://\\S+)\\)',
+      format = '$1',
+      highlight = 1,
+    },
+    -- Matches: a URL in brackets: [URL]
+    {
+      regex = '\\[(\\w+://\\S+)\\]',
+      format = '$1',
+      highlight = 1,
+    },
+    -- Matches: a URL in curly braces: {URL}
+    {
+      regex = '\\{(\\w+://\\S+)\\}',
+      format = '$1',
+      highlight = 1,
+    },
+    -- Matches: a URL in angle brackets: <URL>
+    {
+      regex = '<(\\w+://\\S+)>',
+      format = '$1',
+      highlight = 1,
+    },
+    -- Then handle URLs not wrapped in brackets
+    {
+      -- Before
+      --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+      --format = '$0',
+      -- After
+      regex = '[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)',
+      format = '$1',
+      highlight = 1,
+    },
+    -- implicit mailto link
+    {
+      regex = '\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b',
+      format = 'mailto:$0',
+    },
+  },
 }
-
--- config.window_background_image = "~/Documents/img-backgrounds/sacred-geometry.jpeg"
---
--- config.window_background_gradient = {
---   orientation = 'Vertical',
---   colors = {
---     '#1E1E2F',
---     '#1E1E2F',
---   },
---   blend = 'Rgb',
---   interpolation = 'Linear',
--- }
-
-config.window_padding = {
-	left = 2.5,
-	right = 2.5,
-	top = 2.5,
-	bottom = 4.5,
-}
--- config.window_padding = {
---   left = 55,
---   right = 44,
---   top = 44,
---   bottom = 22,
--- }
-
--- config.default_prog = { 'zel', '-l', 'welcome'}
-
-config.keys = {
-	{
-		key = "c",
-		mods = "CMD",
-		action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
-	},
-	{
-		-- Disable Cmd+T (or Super+T on some systems)
-		key = "t",
-		mods = "CMD",
-		action = wezterm.action.DisableDefaultAssignment,
-	},
-}
-
 return config
