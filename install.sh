e171956e9a20d02ed647467f2098eea6f9a30de6
BORBA JR, W <wborbajr@gmail.com>
2025-09-17 21:00:09 -0300

    install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+
+# Install fd
+cargo install fd-find
+
+# Install ripgrep
+$ cargo install ripgrep
+
+# Install zoxide
+cargo install zoxide --locked
+
+# Install yazi
+cargo install --force yazi-build
+
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Install helix
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo nala update
+sudo nala install helix
+
+
