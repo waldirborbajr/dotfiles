5de95d07f87014e5a0d7eeb713f783b99392a2b3
BORBA JR, W <wborbajr@gmail.com>
2025-09-18 22:20:14 -0300

    chore: completation complement

--- zshrc/completion.zsh
+++ zshrc/completion.zsh
@@ -1,14 +1,69 @@
 # Completions avançadas
 if typeset -f compinit >/dev/null; then
   # Case-insensitive tab completion
-  zstyle ':completion:*' completer _complete _ignored _approximate
   zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
   zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-  zstyle ':completion:*' menu select
   zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
   zstyle ':completion:*' verbose true
 
   # Enable cache for completions
-  zstyle ':completion:*' use-cache on
   zstyle ':completion:*:git:*' group-order 'main commands' 'alias commands' 'external commands'
+#-------
+
+        # General completion behavior
+        zstyle ':completion:*' completer _extensions _complete _approximate
+
+        # Use cache
+        zstyle ':completion:*' use-cache on
+#        zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+
+        # Complete the alias
+        zstyle ':completion:*' complete true
+
+        # Autocomplete options
+        zstyle ':completion:*' complete-options true
+
+        # Completion matching control
+        zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+        zstyle ':completion:*' keep-prefix true
+
+        # Group matches and describe
+        zstyle ':completion:*' menu select
+        zstyle ':completion:*' list-grouped false
+        zstyle ':completion:*' list-separator ''
+        zstyle ':completion:*' group-name ''
+        zstyle ':completion:*:matches' group 'yes'
+        zstyle ':completion:*:warnings' format '%F{red}%B-- No match for: %d --%b%f'
+        zstyle ':completion:*:messages' format '%d'
+        zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
+        zstyle ':completion:*:descriptions' format '[%d]'
+
+        # Colors
+        zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+
+        # Directories
+        zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+        zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
+        zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
+        zstyle ':completion:*:*:-command-:*:*' group-order aliases builtins functions commands
+        zstyle ':completion:*' special-dirs true
+        zstyle ':completion:*' squeeze-slashes true
+
+        # Sort
+        zstyle ':completion:*' sort false
+        zstyle ":completion:*:git-checkout:*" sort false
+        zstyle ':completion:*' file-sort modification
+        zstyle ':completion:*:eza' sort false
+        zstyle ':completion:complete:*:options' sort false
+        zstyle ':completion:files' sort false
+
+        # fzf-tab
+        zstyle ':fzf-tab:complete:*:*' fzf-preview 'preview $realpath'
+        zstyle ':fzf-tab:complete:kill:argument-rest' fzf-preview 'ps --pid=$word -o cmd --no-headers -w -w'
+        zstyle ':fzf-tab:complete:kill:argument-rest' fzf-flags '--preview-window=down:3:wrap'
+        zstyle ':fzf-tab:*' fzf-command fzf
+        zstyle ':fzf-tab:*' fzf-pad 4
+        zstyle ':fzf-tab:*' fzf-min-height 100
+        zstyle ':fzf-tab:*' switch-group ',' '.'
+
 fi
