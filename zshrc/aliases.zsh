f55b8a4f8e92c6667fb3cc31e1674d7bd3a0f58b
Waldir Borba Junior <wborbajr@gmail.com>
2025-09-24 08:35:51 -0300

    chore: git aliases

--- zshrc/aliases.zsh
+++ zshrc/aliases.zsh
@@ -5,3 +5,13 @@ alias la="eza -lag --icons --group-directories-first"
 alias gs='git status'
 alias dc='docker compose'
 alias rmvim="rm -rf ~/.config/nvim ~/.local/state/nvim ~/.local/share/nvim"
+
+alias g='git'
+alias gst='git status'
+alias gco='git checkout'
+alias gcmsg='git commit -m'
+alias ga='git add .'
+alias gaa='git add --all'
+alias gp='git push'
+alias gl='git pull'
+alias glo='git log --oneline --decorate --graph'
