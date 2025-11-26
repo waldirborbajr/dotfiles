337667365807c14e4b84e5bb72ec984e02752a80
BORBA JR, W <wborbajr@gmail.com>
2025-11-26 20:27:13 -0300

    chore:aliases

--- zshrc/aliases.zsh
+++ zshrc/aliases.zsh
@@ -7,15 +7,37 @@ alias ....="cd ../../.."
 alias ll="eza -lg --icons --group-directories-first"
 alias la="eza -lag --icons --group-directories-first"
 
-# Git shortcuts
-alias g="git"
-alias gs="git status -s"
-alias ga="git add ."
-alias gc="git commit -m"
-alias gp="git push"
-alias gl="git pull"
-alias glog="git log --oneline --graph --all"
-alias gh-create='gh repo create --private --source=. --remote=origin && git push -u --all && gh browse'
+# Aliases: git
+alias ga='git add'
+alias gap='ga --patch'
+alias gb='git branch'
+alias gba='gb --all'
+alias gc='git commit'
+alias gca='gc --amend --no-edit'
+alias gce='gc --amend'
+alias gco='git checkout'
+alias gcl='git clone --recursive'
+alias gd='git diff --output-indicator-new=" " --output-indicator-old=" "'
+alias gds='gd --staged'
+alias gi='git init'
+alias gl='git log --graph --all --pretty=format:"%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n"'
+alias gm='git merge'
+alias gn='git checkout -b'  # new branch
+alias gp='git push'
+alias gr='git reset'
+alias gs='git status --short'
+alias gu='git pull'
+alias ghc='gh repo create --private --source=. --remote=origin && git push -u --all && gh browse'
+
+# Aliases: git
+alias git-notes='git -C ~/notes'
+alias git-when='git -C ~/.when'
+alias git-ledger='git -C ~/.ledger'
+
+alias sync-commit-notes='git-notes add --all; git-notes commit --message sync; git-notes pull; git-notes push'
+
+alias cj-pull='pass git pull; git-notes pull; git-when pull; git-ledger pull'
+alias cj-status='pass git status; git-notes status; git-when status; git-ledger status'
 
 # System
 alias dc='docker compose'
@@ -25,34 +47,20 @@ alias xterm="sudo update-alternatives --config x-terminal-emulator"
 
 alias v="nvim"
 alias y="yazi"
+alias y="lazygit"
 
-# # Aliases úteis
-# # alias ll='ls -la --color=auto'
-# alias ll="eza -lg --icons --group-directories-first"
-# alias la="eza -lag --icons --group-directories-first"
-# alias gs='git status'
-# alias dc='docker compose'
-# alias rmvim="rm -rf ~/.config/nvim ~/.local/state/nvim ~/.local/share/nvim"
-#
-# alias gcmsg='git commit -m'
-# alias gaa='git add --all'
-# alias gp='git push'
-# alias gl='git pull'
-# # git aliases
-# alias gt="git"
-# alias ga="git add ."
-# alias gs="git status -s"
-# alias gc='git commit -m'
-# alias glog='git log --oneline --graph --all'
-# alias gh-create='gh repo create --private --source=. --remote=origin && git push -u --all && gh browse'
-#
-# alias reboot="systemctl reboot"
-# alias xterm="sudo update-alternatives --config x-terminal-emulator"
-#
-# alias ".."="cd .."
-# alias "..."="cd ../.."
-# alias "...."="cd ../../.."
-# alias "....."="cd ../../../.."
-#
-# alias rustupdate="rustup self update"
-#
+gcm() { git commit --message "$*" }
+
+# Aliases: docker
+alias dps='docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"'
+alias dl='docker logs --tail=100'
+alias dc='docker compose'
+
+# Aliases: tmux
+alias ta='tmux attach'
+alias tl='tmux list-sessions'
+alias tn='tmux new-session -s'
+
+# Aliases: rg
+alias rg="rg --hidden --smart-case --glob='!.git/' --no-search-zip --trim --colors=line:fg:black --colors=line:style:bold --colors=path:fg:magenta --colors=match:style:nobold"
+
