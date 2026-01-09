14688d3d040d507ceffa8214be8ceb9f41f43a16
BORBA JR W <wborbajr@gmail.com>
2026-01-09 20:35:23 -0300

    modified:   nvim/.config/nvim/lua/config/options.lua

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,3 +4,34 @@
 
 vim.g.snacks_animate = false
 -- vim.g.snacks_scroll = false
+
+-- backup
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.local/state/nvim/undo"
+vim.opt.undofile = true
+
+-- open splits in a more natural direction
+-- https://vimtricks.com/p/open-splits-more-naturally/
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- indent
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- search
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+
+-- colors
+vim.opt.termguicolors = true
+vim.g.syntax = "enable"
+vim.o.winblend = 0
+
+-- clipboard
+vim.opt.clipboard = ""
