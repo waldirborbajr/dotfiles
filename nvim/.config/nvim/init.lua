9d5072d5ecda42be87c4b1a663f74d49f614012f
Waldir Borba Junior <wborbajr@gmail.com>
2026-04-17 19:18:52 -0300

    chore: nvim 0.12

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,89 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.pack.add {
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/neovim/nvim-lspconfig',
+    'https://github.com/stevearc/oil.nvim',
+    'https://github.com/y9san9/y9nika.nvim',
+    'https://github.com/wakatime/vim-wakatime',
+    'https://github.com/brenoprata10/nvim-highlight-colors',
+}
+
+vim.cmd.packadd('cfilter')
+vim.cmd.packadd('nvim.undotree')
+vim.cmd.packadd('nvim.difftool')
+
+vim.g.mapleader = ','
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.colorcolumn = '80'
+vim.opt.textwidth = 80
+vim.opt.completeopt = 'menu,menuone,fuzzy,noinsert'
+vim.opt.swapfile = false
+vim.opt.confirm = true
+vim.opt.linebreak = true
+vim.opt.termguicolors = true
+vim.opt.wildoptions:append { 'fuzzy' }
+vim.opt.path:append { '**' }
+vim.opt.smoothscroll = true
+vim.opt.grepprg = 'rg --vimgrep --no-messages --smart-case'
+vim.opt.statusline = '[%n] %<%f %h%w%m%r%=%-14.(%l,%c%V%) %P'
+
+vim.cmd.colorscheme('y9nika')
+
+-- disable mouse popup yet keep mouse enabled
+vim.cmd [[
+  aunmenu PopUp
+  autocmd! nvim.popupmenu
+]]
+
+-- Only highlight with treesitter
+vim.cmd('syntax off')
+
+require("nvim-highlight-colors").setup {
+    render = 'virtual',
+    virtual_symbol = '⚫︎',
+    virtual_symbol_suffix = '',
+}
+require('oil').setup {
+    keymaps = { ['<C-h>'] = false },
+    columns = { 'size', 'mtime' },
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+}
+
+-- Keymaps
+
+vim.keymap.set('n', '<leader><leader>', ':Oil<CR>', { silent = true })
+
+vim.keymap.set('n', '<leader>a', function()
+    vim.cmd('$argadd %')
+    vim.cmd('argdedup')
+end)
+vim.keymap.set('n', '<C-h>', function() vim.cmd('silent! 1argument') end)
+vim.keymap.set('n', '<C-j>', function() vim.cmd('silent! 2argument') end)
+vim.keymap.set('n', '<C-k>', function() vim.cmd('silent! 3argument') end)
+vim.keymap.set('n', '<C-n>', function() vim.cmd('silent! 4argument') end)
+vim.keymap.set('n', '<C-m>', function() vim.cmd('silent! 5argument') end)
+
+-- Autocommands
+
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function() pcall(vim.treesitter.start) end,
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        vim.o.signcolumn = 'yes:1'
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        if client:supports_method('textDocument/completion') then
+            vim.o.complete = 'o,.,w,b,u'
+            vim.o.completeopt = 'menu,menuone,popup,noinsert'
+            vim.lsp.completion.enable(true, client.id, args.buf)
+        end
+    end
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function() vim.highlight.on_yank() end,
+})
