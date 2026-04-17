1bd591a5d92d0d3f110607381699ece85cda8b99
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-04-17 00:06:34 -0300

    Update autocmds.lua

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -125,3 +125,20 @@ autocmd("FileType", {
     end
   end,
 })
+
+------------- https://github.com/FractalCodeRicardo
+vim.opt.wildmode = "noselect"
+vim.api.nvim_create_autocmd("CmdlineChanged", {
+  pattern = ":",
+  callback = function ()
+    vim.fn.wildtrigger()
+  end
+})
+
+function _G.my_find(text, _)
+  local files = vim.fn.glob("**/*", true, true)
+
+  return vim.fn.matchfuzzy(files, text)
+end
+
+vim.opt.findfunc = "v:lua.my_find"
