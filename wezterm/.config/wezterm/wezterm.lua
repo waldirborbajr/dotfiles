b7e96fd07a03782f5b999cd4b483ba51b914a3ae
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-04-17 19:35:12 -0300

    Update wezterm.lua

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -32,7 +32,6 @@ config.cursor_blink_rate = 0
 config.color_scheme = "Catppuccin Macchiato"
 config.window_decorations = "RESIZE"
 config.window_padding = { left = 6, right = 6, top = 6, bottom = 16 }
-config.window_startup_mode = "Fullscreen"
 
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
