126e168a0d7564da9ba0e95f33ee09c603a442a6
Waldir Borba Junior <wborbajr@gmail.com>
2026-04-18 14:45:16 -0300

    chore::nvim tiny

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,39 +1,179 @@
--- TODO: add
--- - mason (for non nix systems)
+-- 0. CHECKS ===========================================================================================================
 
-vim.pack.add({
-	{ src = "https://github.com/aylur/nucharm.nvim" },
-	{ src = "https://github.com/L3MON4D3/LuaSnip" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/stevearc/conform.nvim" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-	{ src = "https://github.com/saghen/blink.cmp", version = "v1.8.0" },
-	{ src = "https://github.com/akinsho/bufferline.nvim", version = "v4.9.1" },
-	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
-	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
-	{ src = "https://github.com/nvim-mini/mini.icons" },
-	{ src = "https://github.com/folke/ts-comments.nvim" },
-	{ src = "https://github.com/nvim-mini/mini.pairs" },
-	{ src = "https://github.com/folke/which-key.nvim" },
-	{ src = "https://github.com/folke/todo-comments.nvim" },
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/christoomey/vim-tmux-navigator" },
-	{ src = "https://github.com/folke/snacks.nvim" },
-	{ src = "https://github.com/MunifTanjim/nui.nvim" },
-	{ src = "https://github.com/folke/noice.nvim" },
-	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
-	{ src = "https://github.com/windwp/nvim-ts-autotag.git" },
-	{ src = "https://github.com/b0o/SchemaStore.nvim.git" },
+-- nvim 0.12 is required for `vim.pack`.
+if vim.fn.has "nvim-0.12" == 0 then
+  error "[ERROR] Requires nvim 0.12"
+end
+
+
+-- 1. OPTIONS ==========================================================================================================
+
+vim.g.mapleader       = " "
+vim.g.maplocalleader  = "\\"
+
+vim.o.number          = true
+vim.o.relativenumber  = true
+
+vim.o.autoindent      = true      -- copy indent from current line when starting a new line
+vim.o.smartindent     = true      -- do smart autoindenting when starting a new line
+vim.o.expandtab       = true      -- use spaces instead of tabs
+vim.o.shiftwidth      = 2         -- size of indent
+vim.o.tabstop         = 2         -- number of spaces tabs count for
+vim.o.smarttab        = true      -- a <Tab> in front of a line inserts blanks according to 'shiftwidth'
+vim.o.shiftround      = true      -- round indent
+
+vim.o.ignorecase      = true      -- ignore case while searching
+vim.o.smartcase       = true      -- override the 'ignorecase' option if the search pattern contains uppercase characters
+vim.o.hlsearch        = true      -- highlight search matches
+vim.o.incsearch       = true      -- highlight search matches while typing search command
+
+vim.o.splitright      = true      -- vertical splits open on the right
+vim.o.splitbelow      = true      -- horizontal splits open below
+
+vim.o.scrolloff       = 8
+
+vim.o.cursorline      = true
+vim.o.signcolumn      = "yes"
+vim.o.statuscolumn    = "%=%{v:relnum == 0 ? v:lnum : v:relnum} %s"
+
+
+
+vim.o.list            = true      -- show invisible characters (e.g. trailing spaces)
+
+vim.o.exrc            = true
+
+vim.o.wrap            = false
+
+
+-- 2. PLUGIN INSTALLATION ==============================================================================================
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    if ev.data.spec.name == "nvim-treesitter" and ev.data.kind == "update" --[[or ev.data.kind == "install"]] then
+      local ok, _ = pcall(function() require "nvim-treesitter" .update "all" end)
+      if not ok then vim.notify "[ERROR] Failed to update nvim-treesitter parsers" end
+    end
+  end,
 })
 
--- config
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
+vim.pack.add {
+  -- appearance
+  "https://github.com/rebelot/kanagawa.nvim",
+  -- "https://github.com/echasnovski/mini.icons",
 
--- plugins
-require("plugins.lualine")
-require("plugins.misc")
-require("plugins.treesitter")
-require("plugins.todolist")
-require("plugins.lang")
+  -- navigation
+  "https://github.com/stevearc/oil.nvim",
+  -- "https://github.com/ibhagwan/fzf-lua",
+
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+  "https://github.com/neovim/nvim-lspconfig", -- data only
+}
+
+
+-- 3. PLUGIN SETUP =====================================================================================================
+
+-- 3.a. kanagawa.nvim (colorscheme) ------------------------------------------------------------------------------------
+
+require "kanagawa" .setup { ---@type KanagawaConfig
+  colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
+
+  ---@param colors KanagawaColors
+  overrides = function(colors)
+    local theme = colors.theme
+
+    return {
+      -- dark popup menus
+      Pmenu = { bg = theme.ui.bg_p1 },
+      PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+      PmenuSbar = { bg = theme.ui.bg_m1 },
+      PmenuThumb = { bg = theme.ui.bg_p2 },
+    }
+  end,
+}
+
+vim.cmd.colorscheme "kanagawa"
+
+
+-- 3.b. oil.nvim (file explorer) ---------------------------------------------------------------------------------------
+
+require "oil" .setup {
+  columns = {
+    "icon",
+    "permissions",
+    "size",
+    "mtime",
+  },
+  view_options = {
+    show_hidden = true,
+  },
+  -- Skip the confirmation popup for simple operations (:h oil.skip_confirm_for_simple_edits)
+  skip_confirm_for_simple_edits = true,
+  float = {
+    border = "solid",
+    max_width = 0.8,
+    max_height = 0.6,
+  },
+}
+
+vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Oil" })
+-- vim.keymap.set("n", "-", require "oil" .open_float, { desc = "Oil (Float)" })
+
+
+-- 3.c. nvim-treesitter (treesitter parser management) -----------------------------------------------------------------
+
+require "nvim-treesitter" .install {
+  "c",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "query",
+  "vim",
+  "vimdoc",
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Enable treesitter highlighting and indents",
+  callback = function(args)
+    local filetype = args.match
+    local lang = vim.treesitter.language.get_lang(filetype)
+    if lang and vim.treesitter.language.add(lang) then
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      vim.treesitter.start()
+    end
+  end
+})
+
+
+-- 4. LSP ==============================================================================================================
+
+vim.lsp.enable {
+  "lua_ls",
+}
+
+
+-- 5. KEYMAPS ==========================================================================================================
+
+-- 5.a. General --------------------------------------------------------------------------------------------------------
+
+vim.keymap.set({ "n", "i", "s" }, "<esc>", function()
+  vim.snippet.stop()  -- exit current snippet (native snippets only)
+  vim.cmd "noh"       -- clear search highlighting
+  return "<esc>"      -- standard esc behaviour
+end, { expr = true, desc = "Escape+" }) -- expr to make sure "<esc>" is actually evaluated
+
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Write" })
+vim.keymap.set("n", "<leader>x", "<cmd>x<cr>", { desc = "Write & Quit" })
+
+
+-- 5.b. Navigate Splits ------------------------------------------------------------------------------------------------
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Focus left pane" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Focus lower pane" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Focus upper pane" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Focus right pane" })
+
+
+-- 5.c. Open Splits ----------------------------------------------------------------------------------------------------
+
+vim.keymap.set("n", "<leader>-", "<C-w>s", { desc = "Split below" })
+vim.keymap.set("n", "<leader>|", "<C-w>v", { desc = "Split right" })
