6ec3751f067538743d111385d11ab833489c7ee3
Waldir Borba Junior <wborbajr@gmail.com>
2026-04-19 20:15:23 -0300

    chore: wip

--- nvim/.config/nvim/lua/borba/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/borba/plugins/lsp/mason.lua
@@ -18,28 +18,28 @@ mason_lspconfig.setup({
     -- servers for mason to install
     ensure_installed = {
         "lua_ls",
-        "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
+        -- "ts_ls",
+        -- "html",
+        -- "cssls",
+        -- "tailwindcss",
         "gopls",
-        "angularls",
-        "astro",
-        "emmet_ls",
-        "emmet_language_server",
+        -- "angularls",
+        -- "astro",
+        -- "emmet_ls",
+        -- "emmet_language_server",
         "marksman",
     },
 })
 
 mason_tool_installer.setup({
     ensure_installed = {
-        "biome",
-        "prettier",
+        -- "biome",
+        -- "prettier",
         "stylua",
         "isort",
-        "pylint",
-        "clangd",
-        "denols",
+        -- "pylint",
+        -- "clangd",
+        -- "denols",
     },
 
 })
