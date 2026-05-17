c36931ecad9ac899b870d2c75e72bf331f286f4f
Waldir Borba Junior <wborbajr@gmail.com>
2026-05-17 11:35:13 -0300

    chore: enabling go and rust

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -734,9 +734,9 @@ do
   ---@type table<string, vim.lsp.Config>
   local servers = {
     -- clangd = {},
-    -- gopls = {},
+    gopls = {},
     -- pyright = {},
-    -- rust_analyzer = {},
+    rust_analyzer = {},
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
     --    https://github.com/pmizio/typescript-tools.nvim
