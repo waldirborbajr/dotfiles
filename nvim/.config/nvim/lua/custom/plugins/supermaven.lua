854b9d43ba40e95711a7ade77c4b752f3e205455
Waldir Borba Junior <wborbajr@gmail.com>
2026-05-17 11:14:40 -0300

    format: fix formatting

--- nvim/.config/nvim/lua/custom/plugins/supermaven.lua
+++ nvim/.config/nvim/lua/custom/plugins/supermaven.lua
@@ -1,21 +1,18 @@
-
 vim.pack.add { 'https://github.com/supermaven-inc/supermaven-nvim' }
 
-
-  require('supermaven-nvim').setup {
-    keymaps = {
-      accept_suggestion = '<Tab>',
-      clear_suggestion = '<C-]>',
-      accept_word = '<C-j>',
-    },
-    ignore_filetypes = { cpp = true },
-    color = {
-      suggestion_color = '#ffffff',
-      cterm = 244,
-    },
-    log_level = 'info',
-    disable_inline_completion = false,
-    disable_keymaps = false,
-    condition = function() return false end,
-  }
-
+require('supermaven-nvim').setup {
+  keymaps = {
+    accept_suggestion = '<Tab>',
+    clear_suggestion = '<C-]>',
+    accept_word = '<C-j>',
+  },
+  ignore_filetypes = { cpp = true },
+  color = {
+    suggestion_color = '#ffffff',
+    cterm = 244,
+  },
+  log_level = 'info',
+  disable_inline_completion = false,
+  disable_keymaps = false,
+  condition = function() return false end,
+}
