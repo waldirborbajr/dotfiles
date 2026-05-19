253b91455fae160fc1bd61a920deb02d79d0fa81
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-05-19 11:55:22 -0300

    Update autocmds.lua

--- nvim/.config/nvim/lua/custom/autocmds.lua
+++ nvim/.config/nvim/lua/custom/autocmds.lua
@@ -1,20 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Default autocmds:
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 --
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
+-- Add custom autocmds here using:
+-- vim.api.nvim_create_autocmd(...)
 --
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
+-- Default LazyVim autocmd groups can be removed with:
+-- vim.api.nvim_del_augroup_by_name("lazyvim_<group_name>")
 
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
--- Restore cursor position
+-- ============================================================================
+-- Restore last cursor position when reopening a file
+-- ============================================================================
 autocmd("BufReadPost", {
   group = augroup("last_location", { clear = true }),
-  desc = "Go to last cursor position",
+  desc = "Restore last cursor position",
   callback = function(args)
     local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
     local line_count = vim.api.nvim_buf_line_count(args.buf)
@@ -25,16 +27,20 @@ autocmd("BufReadPost", {
   end,
 })
 
--- Disable auto comment continuation
+-- ============================================================================
+-- Disable automatic comment continuation
+-- ============================================================================
 autocmd("FileType", {
   group = augroup("no_auto_comment", { clear = true }),
-  desc = "Disable auto comment continuation",
+  desc = "Disable automatic comment continuation",
   callback = function()
     vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
 })
 
--- Disable next line comments
+-- ============================================================================
+-- Ensure comment continuation is disabled on buffer enter
+-- ============================================================================
 autocmd("BufEnter", {
   desc = "Disable next line comments",
   callback = function()
@@ -43,18 +49,22 @@ autocmd("BufEnter", {
   end,
 })
 
--- Markdown settings
+-- ============================================================================
+-- Markdown specific settings
+-- ============================================================================
 autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
-  desc = "Markdown settings",
-  pattern = { "*.mdx", "*.md" },
+  desc = "Apply markdown settings",
+  pattern = { "*.md", "*.mdx" },
   callback = function()
     vim.cmd([[set filetype=markdown wrap linebreak nolist nospell]])
   end,
 })
 
--- Fix syntax highlighting for special buffers
+-- ============================================================================
+-- Fix syntax highlighting for special buffer types
+-- ============================================================================
 autocmd("FileType", {
-  desc = "Fix syntax highlighting",
+  desc = "Fix syntax highlighting for special buffers",
   pattern = {
     "gitsendemail",
     "conf",
@@ -68,9 +78,11 @@ autocmd("FileType", {
   end,
 })
 
--- Clear trailing whitespace on write
+-- ============================================================================
+-- Remove trailing whitespace before saving files
+-- ============================================================================
 autocmd("BufWritePre", {
-  desc = "Clear trailing whitespace on write",
+  desc = "Remove trailing whitespace on save",
   callback = function()
     local view = vim.fn.winsaveview()
 
@@ -80,17 +92,21 @@ autocmd("BufWritePre", {
   end,
 })
 
--- Highlight selection on yank
+-- ============================================================================
+-- Highlight yanked text
+-- ============================================================================
 autocmd("TextYankPost", {
-  desc = "Highlight selection on yank",
+  desc = "Highlight yanked text",
   callback = function()
     vim.hl.on_yank()
   end,
 })
 
--- CD to terminal cwd on enter
+-- ============================================================================
+-- Change current working directory to terminal cwd
+-- ============================================================================
 autocmd({ "BufEnter", "TermEnter", "TermLeave" }, {
-  desc = "CD to terminal cwd on enter",
+  desc = "Sync cwd with terminal buffer",
   pattern = "term://*",
   callback = function()
     local cwd = vim.fn.resolve("/proc/" .. vim.b.terminal_job_pid .. "/cwd")
@@ -101,41 +117,63 @@ autocmd({ "BufEnter", "TermEnter", "TermLeave" }, {
   end,
 })
 
--- Restore scrolloff when leaving a terminal buffer
-autocmd("BufLeave", {
-  desc = "Restore scrolloff when leaving a terminal buffer",
+-- ============================================================================
+-- Save and restore scrolloff for terminal buffers
+-- ============================================================================
+autocmd("TermEnter", {
+  desc = "Save scrolloff before entering terminal",
   pattern = "term://*",
   callback = function()
-    vim.o.scrolloff = options.scrolloff
+    vim.b.saved_scrolloff = vim.o.scrolloff
+    vim.o.scrolloff = 0
   end,
 })
 
--- Try to start treesitter for supported filetypes
+autocmd("BufLeave", {
+  desc = "Restore scrolloff after leaving terminal",
+  pattern = "term://*",
+  callback = function()
+    if vim.b.saved_scrolloff ~= nil then
+      vim.o.scrolloff = vim.b.saved_scrolloff
+    end
+  end,
+})
+
+-- ============================================================================
+-- Start Tree-sitter automatically for supported filetypes
+-- ============================================================================
 autocmd("FileType", {
-  desc = "Try to start treesitter for supported filetypes",
+  desc = "Start Tree-sitter automatically",
   callback = function()
     pcall(vim.treesitter.start)
   end,
 })
 
--- Set keywordprg for vim files
+-- ============================================================================
+-- Improve help lookup for Vim script files
+-- ============================================================================
 autocmd("FileType", {
-  desc = "Set keywordprg for vim files",
+  desc = "Set keywordprg for Vim files",
   pattern = "vim",
   command = [[setlocal keywordprg=:vert\ help]],
 })
 
--- Set tab to 1 to enable folding by indent
+-- ============================================================================
+-- Configure manual pages for indent-based folding
+-- ============================================================================
 autocmd("FileType", {
-  desc = "Set tab to 1 to enable folding by indent",
+  desc = "Configure man page indentation",
   pattern = "man",
   command = [[setlocal sw=1 ts=1]],
 })
 
--- Unfold folds on buffer enter
+-- ============================================================================
+-- Open all folds automatically when entering a buffer
+-- ============================================================================
 autocmd("BufEnter", {
-  desc = "Unfold folds on buffer enter",
+  desc = "Open all folds on buffer enter",
   command = [[silent! normal zR]],
 })
 
+-- Load custom indentation settings
 require("custom.indent")
