d928083807ba8ab4bea2891daa54fd9cd5062063
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-05-19 11:48:06 -0300

    Update autocmds.lua

--- nvim/.config/nvim/lua/custom/autocmds.lua
+++ nvim/.config/nvim/lua/custom/autocmds.lua
@@ -46,4 +46,59 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function(event) vim.bo[event.buf].syntax = vim.bo[event.buf].filetype end,
 })
 
+-- autocmds {{{
+vim.api.nvim_create_autocmd('BufWritePre', {
+    desc = 'clear trailing whitespace on write',
+    callback = function()
+        local view = vim.fn.winsaveview()
+        vim.cmd([[ %s/\s\+$//e ]])
+        vim.fn.winrestview(view)
+    end,
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'highlight selection on yank',
+    callback = function() vim.hl.on_yank() end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'TermEnter', 'TermLeave' }, {
+    desc = 'cd to terminal cwd on enter',
+    pattern = 'term://*',
+    callback = function()
+        local cwd = vim.fn.resolve('/proc/' .. vim.b.terminal_job_pid .. '/cwd')
+        if vim.fn.isdirectory(cwd) == 1 then
+            vim.fn.chdir(cwd)
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd('BufLeave', {
+    desc = 'restore scrolloff when leaving a terminal buffer',
+    pattern = 'term://*',
+    callback = function() vim.o.scrolloff = options.scrolloff end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    desc = 'try to start treesitter for supported filetypes',
+    callback = function() pcall(vim.treesitter.start) end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    desc = 'set keywordprg for vim files',
+    pattern = 'vim',
+    command = [[ setlocal keywordprg=:vert\ help ]],
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    desc = 'set tab to 1 to enable folding by indent',
+    pattern = 'man',
+    command = [[ setlocal sw=1 ts=1 ]],
+})
+
+vim.api.nvim_create_autocmd('BufEnter', {
+    desc = 'unfold folds on buffer enter',
+    command = [[ silent! normal zR ]],
+})
+-- }}}
+
 require 'custom.indent'
