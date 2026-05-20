5bb26d79c3fbe52904d4130dbf0f2ca0e0cff7e4
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-05-20 12:02:38 -0300

    Update autocmds.lua

--- nvim/.config/nvim.LAZY/lua/config/autocmds.lua
+++ nvim/.config/nvim.LAZY/lua/config/autocmds.lua
@@ -193,3 +193,35 @@ end, {
   desc = "Update all plugins or specific ones (:PackUpdate [plugin1 plugin2])"
 })
 
+
+
+vim.api.nvim_create_user_command("SaveMacro", function (params)
+  local name = params.args
+  local dir = vim.fn.expand("~/.config/nvim/macros/")
+  local file = dir .. name .. ".macro"
+  local content = vim.fn.getreg("q")
+
+  vim.fn.mkdir(dir, "p")
+
+  vim.fn.writefile(
+    {content},
+    file,
+    "a"
+  )
+end, {nargs = 1})
+
+
+vim.api.nvim_create_user_command("LoadMacro", function (params)
+  local name = params.args
+  local dir = vim.fn.expand("~/.config/nvim/macros/")
+  local file = dir .. name .. ".macro"
+
+  local content = vim.fn.readfile(file)
+
+ vim.fn.setreg("q", content)
+end, {nargs = 1})
+
+vim.api.nvim_create_user_command("Reload", function () 
+  vim.cmd(":source $MYVIMRC")
+end
+,{})
