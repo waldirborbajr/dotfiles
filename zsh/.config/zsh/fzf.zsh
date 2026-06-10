2bc7c82dda74d379938cadc20e130cafc7099978
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-06-09 22:46:02 -0300

    Update fzf.zsh

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -20,31 +20,6 @@ export FZF_DEFAULT_OPTS='
 export _FZF_PREVIEW_CMD='bat --color=always --style=plain,numbers --line-range=:500 {}'
 export FZF_CTRL_T_OPTS="--preview '$_FZF_PREVIEW_CMD'"
 
-# Registra os widgets do fzf (fzf-history-widget, fzf-file-widget, etc.)
-# Necessário para que bindings.zsh/zvm_after_init possam fazer bindkey neles.
-# Tenta `fzf --zsh` primeiro (fzf >= 0.48); fallback para arquivos estáticos.
-if command -v fzf &>/dev/null; then
-  if fzf --zsh &>/dev/null; then
-    eval "$(fzf --zsh)"
-  else
-    for _fzf_kb in \
-      /usr/share/fzf/key-bindings.zsh \
-      /usr/share/doc/fzf/examples/key-bindings.zsh \
-      /opt/homebrew/opt/fzf/shell/key-bindings.zsh \
-      /usr/local/opt/fzf/shell/key-bindings.zsh; do
-      [[ -f "$_fzf_kb" ]] && { source "$_fzf_kb"; break; }
-    done
-    for _fzf_comp in \
-      /usr/share/fzf/completion.zsh \
-      /usr/share/doc/fzf/examples/completion.zsh \
-      /opt/homebrew/opt/fzf/shell/completion.zsh \
-      /usr/local/opt/fzf/shell/completion.zsh; do
-      [[ -f "$_fzf_comp" ]] && { source "$_fzf_comp"; break; }
-    done
-    unset _fzf_kb _fzf_comp
-  fi
-fi
-
 # Ctrl+F: file picker excluding hidden files
 _fzf_file_no_hidden() {
   local cmd result
