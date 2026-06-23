a41d531fb650ed59113544c552a923089e7fc805
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-06-23 12:06:04 -0300

    Update init.lua

--- nvim/.config/nvim.FLAKE/init.lua
+++ nvim/.config/nvim.FLAKE/init.lua
@@ -5,6 +5,17 @@ vim.g.maplocalleader = " "
 local scrolloff = math.floor(vim.o.lines / 2) - 3
 vim.opt.scrolloff = scrolloff
 
+require("vim._core.ui2").enable({
+	enable = true,
+	msg = {
+		target = "cmd", -- options: cmd(classic), msg(similar to noice)
+		pager = { height = 1 },
+		msg = { height = 0.5, timeout = 4500 },
+		dialog = { height = 0.5 },
+		cmd = { height = 0.5 },
+	},
+})
+
 require("plugins.init")
 require("config.autocmd")
 require("config.binds")
